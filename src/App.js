2aa64f87347059ccb44c165291165328947dc6d7
Jack Savage <jack.savage@bjss.com>
2021-06-08 23:14:54 +0100

    add delete task & toggle reminder functionality

--- src/App.js
+++ src/App.js
@@ -10,12 +10,33 @@ function App() {
         date: 'June 28th at 4:00pm',
         reminder: false,
     },
+    {
+      id: 2,
+      text: 'Go to the gym',
+      date: 'June 29th at 5:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Win the lottery',
+      date: 'July 5th at 8:00pm',
+      reminder: false,
+    }
 ])
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
 
   return (
     <div className="container">
       <Header title="Task Tracker"/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
+      
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,24 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
 
-function Task({ task }) {
+function Task({ id, text, date, reminder, onDelete, onToggle }) {
     return (
-        <div className='task'>
-            <h3>{task.text}</h3>
-            <p>{task.date}</p>
+        <div className={`task ${reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(id)}>
+            <h3>
+                {text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(id)}/>
+            </h3>
+            <p>{date}</p>
         </div>
     )
 }
 
 Task.propTypes = {
-    task: PropTypes.object.isRequired
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    reminder: PropTypes.bool.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onToggle: PropTypes.func.isRequired,
 }
 
 export default Task
